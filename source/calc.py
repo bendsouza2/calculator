e48d505884e945319d67c6f8a48fba49e9fdc57d
Amir Maula <amirmaula@outlook.com>
2021-11-14 15:54:19 +0000

    Changed the presentation

--- source/calc.py
+++ source/calc.py
@@ -1,5 +1,5 @@
 
-a = input('Enter a number')
-b = input('Enter a number')
+a = input('Enter a number: ')
+b = input('Enter a number: ')
 
-print(int(a) + int(b))
+print(a, '+', b, '=',  int(a) + int(b))
